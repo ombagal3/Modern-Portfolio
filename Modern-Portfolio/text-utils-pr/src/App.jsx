f7bd80a5d7e2c1e5b3c94ec2d444ce9500a203ca
ombagal3 <ombagal8@gmail.com>
2026-03-04 18:41:21 +0530

    first commit

---
+++ Modern-Portfolio/text-utils-pr/src/App.jsx
@@ -0,0 +1,23 @@
+
+import Nav from "./components/Navbar";
+import TextUtils from "./components/TextUtils";
+
+
+
+const App = () => {
+
+
+
+
+  return <>
+
+
+    <Nav/>
+  
+  <TextUtils/>
+  </>
+}
+
+
+
+export default App;
